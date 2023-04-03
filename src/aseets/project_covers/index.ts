6c7b182afd0cff50235265c598ab801f67a9abdc
Volodymyr <volodyaruzhak@gmail.com>
2023-04-03 16:27:22 -0400

    create constants for projects and add images

---
+++ src/aseets/project_covers/index.ts
@@ -0,0 +1,27 @@
+import aghReactjs from './agh-reactjs.png';
+import agh from './agh.png';
+import chevysonic from './chevysonic.png';
+import fitness from './fitness.png';
+import GPT3 from './GPT3.png';
+import madMovies from './mad_movies.png';
+import realEstate from './realestate.png';
+import signOut from './signout.png';
+import tamagotchi from './tamagotchi.png';
+import techCityOshawa from './tech_city_oshawa.png';
+import theSweetGroceries from './theSweetGroceries.png';
+import travel from './travel.png';
+
+export {
+    aghReactjs,
+    agh,
+    chevysonic,
+    fitness,
+    GPT3,
+    madMovies,
+    realEstate,
+    signOut,
+    tamagotchi,
+    techCityOshawa,
+    theSweetGroceries,
+    travel
+}
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,5 @@
+import { aghReactjs, agh, chevysonic, fitness, GPT3, madMovies, realEstate, signOut, tamagotchi, techCityOshawa, theSweetGroceries, travel } from '../aseets/project_covers';
+
 export const navLinks = [
     {
         id: "projects",
@@ -14,5 +16,116 @@ export const navLinks = [
     {
         id: "contactme",
         title: "Contact Me",
+    },
+]
+
+export const projects = [
+    {
+        id: 'aghReact',
+        title: 'AGH React.js',
+        smallDiscription: 'E-commerce tech shop',
+        buildWithTitle: 'Build with:',
+        buildWith: 'React.js / Commerce.jc / Stripe / Rapid API',
+        link: 'https://agh-react-js.netlify.app/',
+        img: aghReactjs
+    },
+    {
+        id: 'chevysonic2018',
+        title: 'Chevy Sonic 2018 LT React.js',
+        smallDiscription: 'Car Specifications and Details',
+        buildWithTitle: 'Build with:',
+        buildWith: 'React.js / Material-UI',
+        link: 'https://chevysonic.vercel.app/',
+        img: chevysonic
+    },
+    {
+        id: 'fitnessapp',
+        title: 'My Fitness React.js',
+        smallDiscription: 'All about fitness',
+        buildWithTitle: 'Build with:',
+        buildWith: 'React.js / Material-UI / Rapid API',
+        link: 'https://fitness-exercises-app-opal.vercel.app/',
+        img: fitness
+    },
+    {
+        id: 'travelapp',
+        title: 'Travel the World React.js',
+        smallDiscription: 'Travel guid app',
+        buildWithTitle: 'Build with:',
+        buildWith: 'React.js / Material-UI / JSON',
+        link: 'https://travel-the-world-react-js.netlify.app/',
+        img: travel
+    },
+    {
+        id: 'realestateapp',
+        title: 'Real Estate Next.js',
+        smallDiscription: 'Buy or Rent Property',
+        buildWithTitle: 'Build with:',
+        buildWith: 'Next.js / Chakra-UI / Rapid API',
+        link: 'https://real-estate-app-next-js.vercel.app/',
+        img: realEstate
+    },
+    {
+        id: 'gpt3app',
+        title: 'GPT-3 OpenAI React.js',
+        smallDiscription: 'Demo of AI Builder',
+        buildWithTitle: 'Build with:',
+        buildWith: 'React.js/Custom CSS',
+        link: 'https://modern-ui-ux-gpt3-neon.vercel.app/',
+        img: GPT3
+    },
+    {
+        id: 'techoshawa',
+        title: 'Teaching City Oshawa Vue.js',
+        smallDiscription: 'Collaborative Project',
+        buildWithTitle: 'Build with:',
+        buildWith: 'Vue.js / JavaScript / JSON',
+        link: 'https://dcdowntownoshawa.netlify.app/',
+        img: techCityOshawa
+    },
+    {
+        id: 'grocerystore',
+        title: 'the Sweet Groceries',
+        smallDiscription: 'Grocery Web Site Store',
+        buildWithTitle: 'Build with:',
+        buildWith: 'PHP / MySQL / JavaScript',
+        link: 'http://tsg.volodymyrdc.ca/#/splash/',
+        img: theSweetGroceries
+    },
+    {
+        id: 'moviewebsite',
+        title: 'Mad Movies / Movie site',
+        smallDiscription: 'Movie Web Site',
+        buildWithTitle: 'Build with:',
+        buildWith: 'PHP / MySQL / JavaScript',
+        link: 'http://madmovies.volodymyrdc.ca/#/splash/',
+        img: madMovies
+    },
+    {
+        id: 'moviewebsite',
+        title: 'Tamagotchi Game',
+        smallDiscription: 'Game using DOM-manipulation',
+        buildWithTitle: 'Build with:',
+        buildWith: 'JavaScript / DOM / GSAP 3 / SVG',
+        link: 'http://tamagotchi.volodymyrdc.ca/',
+        img: tamagotchi
+    },
+    {
+        id: 'equipmentsignout',
+        title: 'Equipment Sign Out Vue.js',
+        smallDiscription: 'Equipment Sign Out Site',
+        buildWithTitle: 'Build with:',
+        buildWith: 'PHP / JavaScript / MySQL / Vue.js',
+        link: 'http://signout.volodymyrdc.ca/',
+        img: signOut
+    },
+    {
+        id: 'equipmentsignout',
+        title: 'Advanced Gaming Hardware',
+        smallDiscription: 'Ecommerce Site',
+        buildWithTitle: 'Build with:',
+        buildWith: 'Custom WordPress theme, WooCommerce',
+        link: 'http://agh.volodymyrdc.ca/',
+        img: agh
     },
 ]
