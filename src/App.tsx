62a7357755d7db17e898650c3537993883cc0666
Volodymyr <volodyaruzhak@gmail.com>
2023-04-04 22:07:36 -0400

    fixed mistakes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { NavBar, Projects, Skills, AboutMe, ContactMe, Footer } from './componen
 function App() {
 
   return (
-    <div className='w-full overflow-hidden transition-opacity duration-1000'>
+    <div className='w-full overflow-hidden'>
       <div id='header' className='sm:px-16 px-6 flex justify-center items-center fixed w-full'>
         <div className='xl:max-w-[1280px] w-full'>
           <NavBar />
         </div>
       </div>
-      <div className='pt-[6rem] xl:max-w-[1280px] w-full'>
+      <div className='pt-[6rem]'>
         <Projects />
         <Skills />
         <AboutMe />
